e49e00926d075e569105189f26c8ca6648d93368
Zsombor <birozsombor4@gmail.com>
2020-03-05 15:13:43 +0100

    I've did several tasks: BMI, Cuboid, DefineBasicInfo, FavoriteNumber, SecondsInADay, Swap, VariableMutation

---
+++ greenfox/birozsombor4/week-01/day-4/Tasks/src/Cuboid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Cuboid {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("a = ");
+        int a = input.nextInt();
+        System.out.println("b = ");
+        int b = input.nextInt();
+        System.out.println("c = ");
+        int c = input.nextInt();
+        System.out.println(a + " , " + b + " , " + c);
+
+        int surfaceArea = (2*a*b) + (2*b*c) + (2*a*c);
+        int volume = a*b*c;
+
+        System.out.println("Surface area: " + surfaceArea);
+        System.out.println("Volume: " + volume);
+    }
+}
---
+++ greenfox/birozsombor4/week-01/day-4/Tasks/src/DefineBasicInfo.java
@@ -0,0 +1,13 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName = "Zsombor Biró";
+        int myAge = 22;
+        double myHeight = 1.71;
+        boolean amIMarried = false;
+
+        System.out.println("My name is: " + myName);
+        System.out.println("My age is: " + myAge);
+        System.out.println("My height is: " + myHeight);
+        System.out.println("Am I married? " + amIMarried);
+    }
+}

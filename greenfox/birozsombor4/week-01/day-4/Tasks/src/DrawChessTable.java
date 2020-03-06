891dded80423f11f9455a7daae585979cb385836
Zsombor <birozsombor4@gmail.com>
2020-03-06 13:32:13 +0100

    Completed tasks: DrawDiamond, DrawSquare, DrawDiagonal, GuessTheNumber, ParametricAvarage, DrawChessTable

---
+++ greenfox/birozsombor4/week-01/day-4/Tasks/src/DrawChessTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DrawChessTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many lines do you want? ");
+        int howManyLines = input.nextInt();
+
+        for (int i = 1; i <= howManyLines; i++) {
+            if (i%2==0){
+                System.out.print(" ");
+            }
+            System.out.print("% % % %");
+            System.out.println("");
+        }
+
+    }
+}
---
+++ greenfox/birozsombor4/week-01/day-4/Tasks/src/ParametricAverage.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ParametricAverage {
+    public static void main(String[] args) {
+        // Write a program that asks for a number.
+        // It would ask this many times to enter an integer,
+        // if all the integers are entered, it should print the sum and average of these
+        // integers like:
+        //
+        // Sum: 22, Average: 4.4
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many integers do you want?");
+        int howManyIntegers = input.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < howManyIntegers; i++) {
+            System.out.print("Type a number: ");
+            int number = input.nextInt();
+            sum += number;
+        }
+        double average = (double) sum / howManyIntegers;
+        System.out.println("SUM: " + sum + " , " + "Average: " + average);
+
+
+
+    }
+}

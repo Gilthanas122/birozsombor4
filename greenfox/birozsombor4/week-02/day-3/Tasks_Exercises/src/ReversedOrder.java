e35e2d071c36d0ffbf5e38945c601b5be8b63ed9
Zsombor <birozsombor4@gmail.com>
2020-03-11 14:02:10 +0100

    ReversedOrder.java

--- greenfox/birozsombor4/week-02/day-3/Tasks_Exercises/src/ReversedOrder.java
+++ greenfox/birozsombor4/week-02/day-3/Tasks_Exercises/src/ReversedOrder.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReversedOrder {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     // Create a method that decrypts reversed-order.txt
+    Path path = Paths.get("files/reversed-order.txt");
+    Path saveLocation = Paths.get("files/reversed-orderFIXED.txt");
+    reverseOrder(path, saveLocation);
+  }
+
+  private static void reverseOrder(Path path, Path saveLocation) throws IOException {
+    List<String> lines = Files.readAllLines(path);
+    List<String> fixedLines = new ArrayList<>();
+    int maxLineIndex = lines.size() - 1;
+    for (int i = 0; i < lines.size(); i++) {
+      fixedLines.add(lines.get(maxLineIndex));
+      maxLineIndex--;
+    }
+    Files.write(saveLocation, fixedLines);
   }
 }

ec9a34dbc93be38dc92fdea5ef5dfc84408b5fa8
Zsombor <birozsombor4@gmail.com>
2020-03-11 13:53:31 +0100

    ReversedLines.java

---
+++ greenfox/birozsombor4/week-02/day-3/Tasks_Exercises/src/ReversedLines.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReversedLines {
+  public static void main(String[] args) throws IOException {
+    // Create a method that decrypts reversed-lines.txt
+    // Create a method that decrypts the duplicated-chars.txt
+    Path path = Paths.get("src/reversed-lines.txt");
+    Path saveLocation = Paths.get("src/reversed-linesFIXED.txt");
+    reverseTheLines(path, saveLocation);
+  }
+
+  private static void reverseTheLines(Path path, Path saveLocation) throws IOException {
+    List<String> lines = Files.readAllLines(path);
+    List<String> fixedLines = new ArrayList<>();
+    for(String line : lines){
+      StringBuilder builder = new StringBuilder(line);
+      fixedLines.add(builder.reverse().toString());
+    }
+    Files.write(saveLocation,fixedLines);
+  }
+}

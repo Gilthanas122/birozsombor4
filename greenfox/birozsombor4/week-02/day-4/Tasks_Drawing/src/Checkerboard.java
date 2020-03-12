c94ef8edf4e5b8a39a4fe660436758d3128c8440
Zsombor <birozsombor4@gmail.com>
2020-03-12 16:06:13 +0100

    Checkerboard

--- greenfox/birozsombor4/week-02/day-4/Tasks_Drawing/src/Checkerboard.java
+++ greenfox/birozsombor4/week-02/day-4/Tasks_Drawing/src/Checkerboard.java
@@ -6,7 +6,27 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Checkerboard {
   public static void mainDraw(Graphics graphics) {
     // Fill the canvas with a checkerboard pattern.
-
+    int size = 20; //in a 320x320 Panel will be 16 lines and 16 columns
+    int x = 0;
+    int y = 0;
+    int howManyCheck = 0;
+    for (int row = 0; row < HEIGHT / size; row++) {
+      for (int column = 0; column < WIDTH / size; column++) {
+        if (howManyCheck % 2 != 0) {
+          graphics.setColor(Color.black);
+          graphics.fillRect(x, y, size, size);
+          howManyCheck++;
+        } else {
+          graphics.setColor(Color.white);
+          graphics.fillRect(x, y, size, size);
+          howManyCheck++;
+        }
+        x += size;
+      }
+      x = 0;
+      y += size;
+      howManyCheck++;
+    }
 
   }
 

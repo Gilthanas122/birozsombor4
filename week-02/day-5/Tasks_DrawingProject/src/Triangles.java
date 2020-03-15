46351e2c96151a9159f2c54d91990e10daf8b319
Zsombor <birozsombor4@gmail.com>
2020-03-15 15:41:56 +0100

    Finished the Triangles.java

--- week-02/day-5/Tasks_DrawingProject/src/Triangles.java
+++ week-02/day-5/Tasks_DrawingProject/src/Triangles.java
@@ -5,10 +5,33 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Triangles {
-  public static void mainDraw(Graphics graphics){
+  public static void mainDraw(Graphics graphics) {
+    int howManyRows = 21;
+    drawTriangles(howManyRows, graphics);
+  }
 
+  public static void drawTriangles(int howManyRows, Graphics graphics) {
+    int size = WIDTH / howManyRows;
+    int startX = 0;
+    int startY = HEIGHT;
+    int x = startX;
+    int y = startY;
+    int completedLines = 0;
+    for (int i = howManyRows; i > 0; i--) {
+      for (int j = i; j > 0; j--) {
+        drawTriangle(x, y, size, graphics);
+        x += size;
+      }
+      completedLines++;
+      x = 0 + (completedLines * (size / 2));
+      y -= size;
+    }
+  }
 
-
+  public static void drawTriangle(int x, int y, int size, Graphics graphics) {
+    graphics.drawLine(x, y, x + size, y);
+    graphics.drawLine(x, y, x + (size / 2), y - size);
+    graphics.drawLine(x + (size / 2), y - size, x + size, y);
   }
 
   // Don't touch the code below

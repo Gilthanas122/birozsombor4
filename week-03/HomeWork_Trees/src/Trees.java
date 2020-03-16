6cd62b41da0da50520420a3f2bd5bb2594e0cefc
Zsombor <birozsombor4@gmail.com>
2020-03-16 14:20:25 +0100

    Homework: Trees

---
+++ week-03/HomeWork_Trees/src/Trees.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Trees {
+  public static void main(String[] args) {
+    HashMap<String, String> tree0 = new HashMap<>();
+    tree0.put("type", "birch");
+    tree0.put("leaf color", "green");
+    tree0.put("age", "1");
+    tree0.put("sex", "male");
+    HashMap<String, String> tree1 = new HashMap<>();
+    tree1.put("type", "maple");
+    tree1.put("leaf color", "dark-green");
+    tree1.put("age", "2");
+    tree1.put("sex", "male");
+    HashMap<String, String> tree2 = new HashMap<>();
+    tree0.put("type", "oak");
+    tree0.put("leaf color", "light-green");
+    tree0.put("age", "3");
+    tree0.put("sex", "male");
+    HashMap<String, String> tree3 = new HashMap<>();
+    tree0.put("type", "pine");
+    tree0.put("leaf color", "green");
+    tree0.put("age", "3");
+    tree0.put("sex", "male");
+    HashMap<String, String> tree4 = new HashMap<>();
+    tree0.put("type", "apple");
+    tree0.put("leaf color", "green");
+    tree0.put("age", "4");
+    tree0.put("sex", "male");
+  }
+}

e05c40515d2eb3394342cebcc2a1a0aabebcd533
Zsombor <birozsombor4@gmail.com>
2020-03-17 17:37:36 +0100

    redo my homework with class and objects(package: redomyhomework)

---
+++ week-03/day-1/HomeWork_Trees/src/redomyhomework/Forest.java
@@ -0,0 +1,11 @@
+package redomyhomework;
+
+import java.awt.Color;
+
+public class Forest {
+  Tree tree1 = new Tree("Birch", Color.GREEN, 1, "male");
+  Tree tree2 = new Tree("Maple", Color.GREEN, 2, "female");
+  Tree tree3 = new Tree("Oak", Color.GREEN, 3, "male");
+  Tree tree4 = new Tree("Pine", Color.GREEN, 4, "female");
+  Tree tree5 = new Tree("Apple", Color.GREEN, 5, "male");
+}
---
+++ week-03/day-1/HomeWork_Trees/src/redomyhomework/Tree.java
@@ -0,0 +1,21 @@
+package redomyhomework;
+
+import java.awt.Color;
+
+public class Tree {
+  String type;
+  Color leafColor;
+  int age;
+  String sex;
+
+  public Tree() {
+
+  }
+
+  public Tree(String type, Color leafColor, int age, String sex) {
+    this.type = type;
+    this.leafColor = leafColor;
+    this.age = age;
+    this.sex = sex;
+  }
+}

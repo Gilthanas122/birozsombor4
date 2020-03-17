3b679a25eb6bf6c377d3296d3ffbd976181bea3c
Zsombor <birozsombor4@gmail.com>
2020-03-17 16:47:05 +0100

    completed package: domino

--- week-03/day-2/Tasks_ClassesAndObjects/src/domino/Dominoes.java
+++ week-03/day-2/Tasks_ClassesAndObjects/src/domino/Dominoes.java
@@ -6,10 +6,24 @@ import java.util.List;
 public class Dominoes {
   public static void main(String[] args) {
     List<Domino> dominoes = initializeDominoes();
+    List<Domino> organizedDomines = new ArrayList<>();
     // You have the list of Dominoes
     // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
     // eg: [2, 4], [4, 3], [3, 5] ...
-
+    organizedDomines.add(dominoes.get(0));
+    dominoes.remove(0);
+    int organizedIndex = 0;
+    for (int i = 0; i < dominoes.size(); i++) {
+      if (organizedDomines.get(organizedIndex).getRightSide() == dominoes.get(i).getLeftSide()) {
+        organizedDomines.add(dominoes.get(i));
+        organizedIndex++;
+        dominoes.remove(i);
+        i = -1;
+      }
+    }
+    for (Domino domino : organizedDomines) {
+      dominoes.add(domino);
+    }
     System.out.println(dominoes);
   }
 

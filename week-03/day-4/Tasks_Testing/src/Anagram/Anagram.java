c185e61c460aca38e8c548f65647c3611bcaacc1
Zsombor <birozsombor4@gmail.com>
2020-03-20 09:57:38 +0100

    I just push everything

--- week-03/day-4/Tasks_Testing/src/Anagram/Anagram.java
+++ week-03/day-4/Tasks_Testing/src/Anagram/Anagram.java
@@ -6,15 +6,20 @@ public class Anagram {
   public boolean AreTheyAnagrams(String input1, String input2) {
     if (input1.length() != input2.length()) {
       return false;
-    } else {
+    }
+    
+    else {
+
       ArrayList<Character> input1Chars = new ArrayList<>();
       for (char c : input1.toCharArray()) {
         input1Chars.add(c);
       }
+
       ArrayList<Character> input2Chars = new ArrayList<>();
       for (char c : input2.toCharArray()) {
         input2Chars.add(c);
       }
+
       for (int i = 0; i < input1Chars.size(); i++) {
         if (input2Chars.contains(input1Chars.get(i))) {
           input2Chars.remove(input2Chars.indexOf(input1Chars.get(i)));
@@ -22,6 +27,7 @@ public class Anagram {
           i = -1;
         }
       }
+
       if (input1Chars.size() == 0 && input2Chars.size() == 0) {
         return true;
       } else {

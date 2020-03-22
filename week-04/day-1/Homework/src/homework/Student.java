da72d831516af8b3bf0e5900d4d6020b01e65e94
Zsombor <birozsombor4@gmail.com>
2020-03-22 10:41:40 +0100

    I've just did the before inheritance exercise.

---
+++ week-04/day-1/Homework/src/homework/Student.java
@@ -0,0 +1,40 @@
+package homework;
+
+public class Student {
+  String name;
+  int age;
+  String gender;
+  String previousOrganization;
+  int skippedDays;
+
+  public Student() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender +
+        " from " + this.previousOrganization + " who skipped " + this.skippedDays + " days from " +
+        "the course already.");
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior developer.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    this.skippedDays = numberOfDays;
+  }
+
+}

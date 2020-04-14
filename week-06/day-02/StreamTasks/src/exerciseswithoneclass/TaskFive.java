998a451c64681bf72f5d553000587616a688f2be
Zsombor <birozsombor4@gmail.com>
2020-04-14 13:47:45 +0200

    added TaskFive

---
+++ week-06/day-02/StreamTasks/src/exerciseswithoneclass/TaskFive.java
@@ -0,0 +1,19 @@
+package exerciseswithoneclass;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TaskFive {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+    int sum = numbers.stream()
+        .filter(i -> i % 2 != 0)
+        .reduce(0, (carry, i) -> carry + i);
+    int sum2 = numbers.stream()
+        .filter(i -> i % 2 != 0)
+        .mapToInt(Integer::intValue)
+        .sum();
+    System.out.println(sum);
+    System.out.println(sum2);
+  }
+}

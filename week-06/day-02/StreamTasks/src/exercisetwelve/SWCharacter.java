3d2690487be256ce4a2172ea7292f13c4d0a5ae6
Zsombor <birozsombor4@gmail.com>
2020-04-15 18:50:43 +0200

    fixed lambda expressions in StarWars

--- week-06/day-02/StreamTasks/src/exercisetwelve/SWCharacter.java
+++ week-06/day-02/StreamTasks/src/exercisetwelve/SWCharacter.java
@@ -22,18 +22,6 @@ public class SWCharacter {
     return mass;
   }
 
-  public String getHairColor() {
-    return hairColor;
-  }
-
-  public String getSkinColor() {
-    return skinColor;
-  }
-
-  public String getEyeColor() {
-    return eyeColor;
-  }
-
   public String getBirthYear() {
     return birthYear;
   }
@@ -45,16 +33,32 @@ public class SWCharacter {
   public SWCharacter(String name, String height, String mass, String hairColor, String skinColor,
                      String eyeColor, String birthYear, String gender) {
     this.name = name;
-    this.height = covertNumberFormat(height);
-    this.mass = covertNumberFormat(mass);
+    this.height = height;
+    this.mass = mass;
     this.hairColor = hairColor;
     this.skinColor = skinColor;
     this.eyeColor = eyeColor;
-    this.birthYear = birthYear;
+    this.birthYear = convertAgeForm(birthYear);
     this.gender = gender;
   }
 
-  private String covertNumberFormat(String number) {
+  private String convertAgeForm(String birthYear) {
+    if (birthYear.contains("BBY")) {
+      birthYear = birthYear.replace("BBY", "");
+    }
+    if (birthYear.contains(".")) {
+      birthYear = birthYear.replace(".", "");
+    }
+    return birthYear;
+  }
+
+  @Override
+  public String toString() {
+    return this.getName();
+  }
+
+  //NEED FOR BEFORE TO THE CLASS CONSTRUCTORS
+  /*private String covertNumberFormat(String number) {
     String holder = number;
     if (number.contains(",")) {
       StringBuilder stringBuilder = new StringBuilder(number);
@@ -66,5 +70,25 @@ public class SWCharacter {
       holder = "unknown";
     }
     return holder;
-  }
+  }*/
+  /*private String convertGender(String gender) {
+    if (!gender.equals("male") && !gender.equals("female")) {
+      gender = "other";
+    }
+    return gender;
+  }*/
+  /*public String getGroup() {
+    if (!this.birthYear.equals("unknown")) {
+      if (Integer.valueOf(this.birthYear) < 21) {
+        return "below 21";
+      } else if (Integer.valueOf(this.birthYear) >= 21 && Integer.valueOf(this.birthYear) < 40) {
+        return "between 21 and 40";
+      } else if (Integer.valueOf(this.birthYear) >= 40) {
+        return "above 40";
+      }
+    } else {
+      return "unknown";
+    }
+    return null;
+  }*/
 }
--- week-06/day-02/StreamTasks/src/exercisetwelve/StarWars.java
+++ week-06/day-02/StreamTasks/src/exercisetwelve/StarWars.java
@@ -6,12 +6,16 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StarWars {
   public static void main(String[] args) {
     System.out.println(nameOfTheHeaviestCharacter("files/swcharacters.csv"));
     System.out.println(averageHeightOfMales("files/swcharacters.csv"));
     System.out.println(averageHeightOfFemales("files/swcharacters.csv"));
+    getAgeDistributionByGender("files/swcharacters.csv")
+        .forEach((k, v) -> System.out.println(k + ":" + v));
   }
 
   public static List<String> getListFromFile(String fileName) {
@@ -36,12 +40,27 @@ public class StarWars {
             line.split(";")[2], line.split(";")[3],
             line.split(";")[4], line.split(";")[5],
             line.split(";")[6], line.split(";")[7]))
-        .filter(character -> !character.getMass().equals("unknown"))
-        .sorted((character1, character2) -> Integer.valueOf(character2.getMass()).compareTo
-            (Integer.valueOf(character1.getMass())))
+        .collect(Collectors.toMap(swCharacter -> swCharacter.getName(), swCharacter -> {
+          String holder = swCharacter.getMass();
+          if (holder.contains(",")) {
+            StringBuilder stringBuilder = new StringBuilder(holder);
+            holder = stringBuilder.delete(holder.indexOf(','), holder.indexOf(',') + 1).toString();
+          } else if (holder.contains(".")) {
+            holder = holder.replace(".", "");
+          }
+          if (holder.isEmpty()) {
+            holder = "unknown";
+          }
+          return holder;
+        }))
+        .entrySet()
+        .stream()
+        .filter(entry -> !entry.getValue().equals("unknown"))
+        .sorted((entry1, entry2) -> Integer.valueOf(entry2.getValue()).compareTo
+            (Integer.valueOf(entry1.getValue())))
         .findFirst()
         .get()
-        .getName();
+        .getKey();
   }
 
   public static double averageHeightOfMales(String fileName) {
@@ -53,8 +72,23 @@ public class StarWars {
             line.split(";")[4], line.split(";")[5],
             line.split(";")[6], line.split(";")[7]))
         .filter(character -> character.getGender().equals("male"))
-        .filter(character -> !character.getHeight().equals("unknown"))
-        .mapToInt(male -> Integer.valueOf(male.getHeight()))
+        .collect(Collectors.toMap(swCharacter -> swCharacter, swCharacter -> {
+          String holder = swCharacter.getHeight();
+          if (holder.contains(",")) {
+            StringBuilder stringBuilder = new StringBuilder(holder);
+            holder = stringBuilder.delete(holder.indexOf(','), holder.indexOf(',') + 1).toString();
+          } else if (holder.contains(".")) {
+            holder = holder.replace(".", "");
+          }
+          if (holder.isEmpty()) {
+            holder = "unknown";
+          }
+          return holder;
+        }))
+        .entrySet()
+        .stream()
+        .filter(entry -> !entry.getValue().equals("unknown"))
+        .mapToInt(entry -> Integer.valueOf(entry.getValue()))
         .average()
         .getAsDouble();
   }
@@ -68,9 +102,102 @@ public class StarWars {
             line.split(";")[4], line.split(";")[5],
             line.split(";")[6], line.split(";")[7]))
         .filter(character -> character.getGender().equals("female"))
+        .collect(Collectors.toMap(swCharacter -> swCharacter, swCharacter -> {
+          String holder = swCharacter.getHeight();
+          if (holder.contains(",")) {
+            StringBuilder stringBuilder = new StringBuilder(holder);
+            holder = stringBuilder.delete(holder.indexOf(','), holder.indexOf(',') + 1).toString();
+          } else if (holder.contains(".")) {
+            holder = holder.replace(".", "");
+          }
+          if (holder.isEmpty()) {
+            holder = "unknown";
+          }
+          return holder;
+        }))
+        .entrySet()
+        .stream()
+        .filter(entry -> !entry.getValue().equals("unknown"))
+        .mapToInt(entry -> Integer.valueOf(entry.getValue()))
+        .average()
+        .getAsDouble();
+  }
+
+  public static Map<String, Map<String, List<SWCharacter>>> getAgeDistributionByGender(String fileName) {
+    List<String> lines = getListFromFile(fileName);
+    return lines.stream()
+        .map(line -> new SWCharacter(line.split(";")[0], line.split(";")[1],
+            line.split(";")[2], line.split(";")[3],
+            line.split(";")[4], line.split(";")[5],
+            line.split(";")[6], line.split(";")[7]))
+        .skip(1)
+        .collect(Collectors.groupingBy(character -> {
+              if (!character.getGender().equals("male") && !character.getGender().equals("female")) {
+                return "other";
+              } else {
+                return character.getGender();
+              }
+            },
+            Collectors.groupingBy(character -> {
+              if (!character.getBirthYear().equals("unknown")) {
+                if (Integer.valueOf(character.getBirthYear()) < 21) {
+                  return "below 21";
+                } else if (Integer.valueOf(character.getBirthYear()) >= 21 && Integer.valueOf(character.getBirthYear()) < 40) {
+                  return "between 21 and 40";
+                } else if (Integer.valueOf(character.getBirthYear()) >= 40) {
+                  return "above 40";
+                }
+              } else {
+                return "unknown";
+              }
+              return null;
+            })));
+  }
+
+    //BEFORE, NEED SOME CHANGES IN SWCharacter
+    /*public static String nameOfTheHeaviestCharacter(String fileName) {
+    List<String> lines = getListFromFile(fileName);
+    return lines.stream()
+        .skip(1)
+        .map(line -> new SWCharacter(line.split(";")[0], line.split(";")[1],
+            line.split(";")[2], line.split(";")[3],
+            line.split(";")[4], line.split(";")[5],
+            line.split(";")[6], line.split(";")[7]))
+        .filter(character -> !character.getMass().equals("unknown"))
+        .sorted((character1, character2) -> Integer.valueOf(character2.getMass()).compareTo
+            (Integer.valueOf(character1.getMass())))
+        .findFirst()
+        .get()
+        .getName();
+  }*/
+
+    /*public static double averageHeightOfMales(String fileName) {
+    List<String> lines = getListFromFile(fileName);
+    return lines.stream()
+        .skip(1)
+        .map(line -> new SWCharacter(line.split(";")[0], line.split(";")[1],
+            line.split(";")[2], line.split(";")[3],
+            line.split(";")[4], line.split(";")[5],
+            line.split(";")[6], line.split(";")[7]))
+        .filter(character -> character.getGender().equals("male"))
         .filter(character -> !character.getHeight().equals("unknown"))
         .mapToInt(male -> Integer.valueOf(male.getHeight()))
         .average()
         .getAsDouble();
-  }
+  }*/
+
+    /* public static Map<String, Map<String, Integer>> getAgeDistributionByGender(String fileName) {
+     List<String> lines = getListFromFile(fileName);
+     lines.stream()
+         .map(line -> new SWCharacter(line.split(";")[0], line.split(";")[1],
+             line.split(";")[2], line.split(";")[3],
+             line.split(";")[4], line.split(";")[5],
+             line.split(";")[6], line.split(";")[7]))
+         .skip(1)
+         .collect(Collectors.groupingBy(SWCharacter::getGender,
+             Collectors.groupingBy(SWCharacter::getGroup)))
+         .forEach((k,v) -> System.out.println(k + ": " + v));
+     return null;
+   }
+ }*/
 }

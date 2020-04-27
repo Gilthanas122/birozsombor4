2e425e7ff8b6ff9d92e0c56a3ef192bea69efbf1
Zsombor <birozsombor4@gmail.com>
2020-04-27 13:36:24 +0200

    finished task connection with mySQL

---
+++ week-08/day-1/Tasks_ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/controller/TodoController.java
@@ -0,0 +1,26 @@
+package com.greenfoxacademy.connectionwithmysql.controller;
+
+import com.greenfoxacademy.connectionwithmysql.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TodoController {
+
+  private TodoRepository todoRepository;
+
+  @Autowired
+  public TodoController(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  @GetMapping(value = {"/", "/list"})
+  public String list(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todolist";
+  }
+
+}

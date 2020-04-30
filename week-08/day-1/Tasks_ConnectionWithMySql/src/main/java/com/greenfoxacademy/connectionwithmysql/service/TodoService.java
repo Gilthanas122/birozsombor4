0bf86b9ef9df7720e067cffc9df7efbc4dfa54b3
Zsombor <birozsombor4@gmail.com>
2020-04-30 14:49:28 +0200

    made some minor changes in TodoService

--- week-08/day-1/Tasks_ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/service/TodoService.java
+++ week-08/day-1/Tasks_ConnectionWithMySql/src/main/java/com/greenfoxacademy/connectionwithmysql/service/TodoService.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;

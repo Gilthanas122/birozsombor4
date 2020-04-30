36768f17e4e4ca455500f3e9ab5e837213080454
Zsombor <birozsombor4@gmail.com>
2020-04-30 09:10:58 +0200

    removed reworked foxclub because it is not complete

--- week-08/day-2/FoxClub_reworked/FoxClub/src/main/java/com/greenfoxacademy/foxclub/repository/ActionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greenfoxacademy.foxclub.repository;
-
-import com.greenfoxacademy.foxclub.model.Action;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ActionRepository extends CrudRepository<Action, Long> {
-}
--- week-08/day-2/FoxClub_reworked/FoxClub/src/main/java/com/greenfoxacademy/foxclub/repository/DrinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy.foxclub.repository;
-
-import com.greenfoxacademy.foxclub.model.Drink;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DrinkRepository extends CrudRepository<Drink, Long> {
-
-  Drink findByNameContainingIgnoreCase(String name);
-}

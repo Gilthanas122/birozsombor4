af4a0a8cb37d0b647ef3771e19310314bf8bfc5f
Zsombor <birozsombor4@gmail.com>
2020-05-04 14:35:47 +0200

    finished task REST: Greeter, AppendA

--- week-09/day-1/Tasks_REST/src/main/java/com/greenfoxacademy/rest/controller/RestController.java
+++ week-09/day-1/Tasks_REST/src/main/java/com/greenfoxacademy/rest/controller/RestController.java
@@ -1,9 +1,12 @@
 package com.greenfoxacademy.rest.controller;
 
+import com.greenfoxacademy.rest.model.AppendA;
 import com.greenfoxacademy.rest.model.Doubling;
 import com.greenfoxacademy.rest.model.Error;
+import com.greenfoxacademy.rest.model.WelcomeMessage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
@@ -15,5 +18,23 @@ public class RestController {
       return ResponseEntity.ok(new Error("Please provide an input!"));
     }
     return ResponseEntity.ok(new Doubling(input));
+  }
+
+  @GetMapping("/greeter")
+  public ResponseEntity<?> getWelcomeMessage(@RequestParam(required = false) String name,
+                                             @RequestParam(required = false) String title) {
+    if (name == null && title == null) {
+      return ResponseEntity.badRequest().body(new Error("Please provide a name and a title!"));
+    } else if (name == null) {
+      return ResponseEntity.badRequest().body(new Error("Please provide a name!"));
+    } else if (title == null) {
+      return ResponseEntity.badRequest().body(new Error("Please provide a title!"));
+    }
+    return ResponseEntity.ok(new WelcomeMessage(name, title));
+  }
+
+  @GetMapping("/appenda/{appendable}")
+  public ResponseEntity<?> getWordWithAppendA(@PathVariable String appendable) {
+    return ResponseEntity.ok(new AppendA(appendable));
   }
 }
---
+++ week-09/day-1/Tasks_REST/src/main/java/com/greenfoxacademy/rest/model/WelcomeMessage.java
@@ -0,0 +1,41 @@
+package com.greenfoxacademy.rest.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = { "name","title" })
+public class WelcomeMessage {
+
+  private String name;
+  private String title;
+  private String welcome_message;
+
+  public WelcomeMessage(String name, String title) {
+    this.name = name;
+    this.title = title;
+    this.welcome_message = "Oh, hi there " + this.name + ", my dear " + this.title + "!";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getWelcome_message() {
+    return welcome_message;
+  }
+
+  public void setWelcome_message(String welcome_message) {
+    this.welcome_message = welcome_message;
+  }
+}

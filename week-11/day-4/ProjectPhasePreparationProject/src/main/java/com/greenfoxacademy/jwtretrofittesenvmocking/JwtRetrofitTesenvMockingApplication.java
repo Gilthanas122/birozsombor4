6e0f772a3f5ccfe17f1640b9508d0e6ca5e01445
Zsombor <birozsombor4@gmail.com>
2020-05-28 13:24:40 +0200

    created project for zero week project

---
+++ week-11/day-4/ProjectPhasePreparationProject/src/main/java/com/greenfoxacademy/jwtretrofittesenvmocking/JwtRetrofitTesenvMockingApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.jwtretrofittesenvmocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRetrofitTesenvMockingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtRetrofitTesenvMockingApplication.class, args);
+  }
+
+}
---
+++ week-11/day-4/ProjectPhasePreparationProject/src/test/java/com/greenfoxacademy/jwtretrofittesenvmocking/JwtRetrofitTesenvMockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.jwtretrofittesenvmocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRetrofitTesenvMockingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
